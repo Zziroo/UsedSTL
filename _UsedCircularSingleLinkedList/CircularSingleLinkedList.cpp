e3ffacf15f363961083dc62de8c43c4500341311
Zziroo <tlslrs1994@naver.com>
2021-11-06 08:11:29 +0900

    Feat : CircularSingleLinkedList class 생성

---
+++ _UsedCircularSingleLinkedList/CircularSingleLinkedList.cpp
@@ -0,0 +1,118 @@
+#include "CircularSingleLinkedList.h"
+
+CircularSingleLinkedList::CircularSingleLinkedList()
+{
+}
+
+CircularSingleLinkedList::CircularSingleLinkedList(const CircularSingleLinkedList& other)
+{
+}
+
+CircularSingleLinkedList& CircularSingleLinkedList::operator=(const CircularSingleLinkedList& rhs)
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+CircularSingleLinkedList::~CircularSingleLinkedList()
+{
+}
+
+int& CircularSingleLinkedList::front()
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+const int& CircularSingleLinkedList::front() const
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+int& CircularSingleLinkedList::back()
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+const int& CircularSingleLinkedList::back() const
+{
+    // TODO: 여기에 return 문을 삽입합니다.
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::before_head()
+{
+    return nullptr;
+}
+
+const CircularSingleLinkedList::Node* CircularSingleLinkedList::before_head() const
+{
+    return nullptr;
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::before_tail()
+{
+    return nullptr;
+}
+
+const CircularSingleLinkedList::Node* CircularSingleLinkedList::before_tail() const
+{
+    return nullptr;
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::head()
+{
+    return nullptr;
+}
+
+const CircularSingleLinkedList::Node* CircularSingleLinkedList::head() const
+{
+    return nullptr;
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::tail()
+{
+    return nullptr;
+}
+
+const CircularSingleLinkedList::Node* CircularSingleLinkedList::tail() const
+{
+    return nullptr;
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::insert_after(Node* pos, int value)
+{
+    return nullptr;
+}
+
+CircularSingleLinkedList::Node* CircularSingleLinkedList::erase_after(Node* pos)
+{
+    return nullptr;
+}
+
+void CircularSingleLinkedList::push_front(int value)
+{
+}
+
+void CircularSingleLinkedList::push_back(int value)
+{
+}
+
+void CircularSingleLinkedList::pop_front()
+{
+}
+
+void CircularSingleLinkedList::pop_back()
+{
+}
+
+bool CircularSingleLinkedList::empty() const
+{
+    return false;
+}
+
+void CircularSingleLinkedList::clear()
+{
+}
+
+bool CircularSingleLinkedList::contains(int value) const
+{
+    return false;
+}
