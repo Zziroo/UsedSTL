1f3657407f06548d6d55a7fcee45da6025cdec85
Zziroo <tlslrs1994@naver.com>
2021-11-25 03:01:51 +0900

    Feat : Binary Search Tree<int> 구현

---
+++ _UsedBinarySearchTree/_UsedBinarySearchTree.cpp
@@ -0,0 +1,96 @@
+﻿// _UsedBinarySearchTree.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <set>
+#include "BinarySearchTree.h"
+
+
+using namespace std;
+
+int main()
+{
+	set<int> set = { 8, 3, 2, 5, 4, 10, 14, 11, 16 };
+
+	// 2, 3, 4, 5, 8, 10, 11, 14, 16
+	for (int data : set)
+	{
+		cout << data << ' ';
+	}
+	cout << "\n";
+
+	if (set.end() != set.find(2)) { cout << "Found 2\n"; }
+
+	if (set.end() == set.find(12)) { cout << "Not Found 12\n"; }
+
+	// 9
+	cout << "Size : " << set.size() << "\n";
+
+	auto iter = set.begin();	// 2
+	++iter;						// 3
+	++iter;						// 4
+
+	set.erase(iter);			// 4를 삭제.
+
+	// 8
+	cout << "Size : " << set.size() << "\n";
+
+	if (set.end() == set.find(4)) { cout << "Not Found 4\n"; }
+
+	// false
+	cout << boolalpha << set.empty() << "\n";
+
+	set.clear();
+
+	// true
+	cout << set.empty() << "\n";
+
+	puts("-------------------------------------");
+
+	BinarySearchTree mySet;
+
+	mySet.insert(8);
+	mySet.insert(3);
+	mySet.insert(2);
+	mySet.insert(5);
+	mySet.insert(4);
+	mySet.insert(10);
+	mySet.insert(14);
+	mySet.insert(11);
+	mySet.insert(16);
+
+	if (mySet.find(2)) { cout << "Found 2\n"; }
+
+	if (mySet.find(12) == nullptr) { cout << "Not Found 12\n"; }
+
+	// 9
+	cout << "Size : " << mySet.size() << "\n";
+
+	mySet.erase(4);
+
+	// 8
+	cout << "Size : " << mySet.size() << "\n";
+
+	if (mySet.find(4) == nullptr) { cout << "Not Found 4\n"; }
+
+	// 8 3 2 5 10 14 11 16
+	mySet.traverseByPreOrder();
+	puts("");
+	// 2 3 5 8 10 11 14 16
+	mySet.traverseByInOrder();
+	puts("");
+	// 2 5 3 11 16 14 10 8
+	mySet.traverseByPostOrder();
+	puts("");
+	// 8 3 10 2 5 14 11 16
+	mySet.traverseByLevelOrder();
+	puts("");
+
+	// 3
+	cout << "HeightBFS : " << mySet.heightBFS() << "\t" << "HeightDFS : " << mySet.heightDFS() << "\n";
+
+	mySet.clear();
+
+	// true
+	cout << mySet.empty() << "\n";
+}
